5bdf84b3fe7f0a1ea55731831fe49156194d72d7
Claudia <claudiainescostamartins@gmail.com>
2019-03-18 16:00:19 +0000

    Changes

--- src/file_parser.c
+++ src/file_parser.c
@@ -5,17 +5,12 @@
 void getInfoFile(char file_name[]){
   char info[MAX_STRING_LENGTH];
   char cmd[MAX_STRING_LENGTH];
-  //FILE *fp;
-  //*fp = FILE *fopen(file_name, r);
 
   //get file type
   strcpy(cmd, "file");
   strcat(cmd, " ");
   strcat(cmd, file_name);
+  //strcat (cmd, " > output.txt");
   system(cmd);
-  //
 
-
-
-  //int fclose( FILE *fp);
 }
