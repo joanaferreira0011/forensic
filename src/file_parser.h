8e03dd2eab5021c8caf2d845193515e6e510b146
joanaferreira0011@gmail.com <joanaferreira0011@gmail.com>
2019-03-29 09:58:08 +0000

    merge

--- src/file_parser.h
+++ src/file_parser.h
@@ -4,4 +4,8 @@
 
 #define MAX_STRING_LENGTH 50
 
+<<<<<<< HEAD
 char* getStringWithInfo(char file_name[]);
+=======
+char* getFileType (char *file_name);
+>>>>>>> 2758f5e66b0cbda8a155568aaadfdf3c1410534f
