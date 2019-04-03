04aabf12df9beee19e7878de9519c83d2333d8d4
joanaferreira0011@gmail.com <joanaferreira0011@gmail.com>
2019-04-03 16:42:40 +0100

    error

--- src/dir_parser.c
+++ src/dir_parser.c
@@ -4,37 +4,59 @@
 #include "path_type.h"
 #include "dir_parser.h"
 #include "file_parser.h"
-int check_dir(char* dir){
+#include <unistd.h>
+#include <sys/types.h>
 
+int check_dir(char* path){
   struct dirent *de;
-   DIR *dr = opendir(dir);
-    path_type type;
-    char* new_dir=(char *)malloc(60*sizeof(char));;
-    type= get_path_type(dir);
+  DIR *dr = opendir(path);
+  path_type type;
+  char* new_path=(char *)malloc(60*sizeof(char));
+  int pid;
+  // type= get_path_type(path);
+  //
+  //   if(type==FILE_PATH){
+  //     parse_file(path);
+  //     free(new_path);
+  //     return 0;
+  //   }
 
-    if(type==FILE_PATH){
-      parse_file(dir);
-      free(new_dir);
-      return 0;
-    }
-
-    else if (dr == NULL)
+    if (dr == NULL)
     {
-        printf("Could not open current directory\n" );
-        free(new_dir);
+        printf("Could not open current pathectory\n" );
+        free(new_path);
         return 0;
     }
 
+
+
    while ((de = readdir(dr)) != NULL){
      if(strcmp(de->d_name, "..") && strcmp(de->d_name, ".")){
-       strcpy(new_dir, dir);
-       strcat(new_dir, "/");
-       strcat(new_dir, de->d_name);
-       check_dir(new_dir);}
+       strcpy(new_path, path);
+       strcat(new_path, "/");
+       strcat(new_path, de->d_name);
+
+       type= get_path_type(new_path);
+       if(type==DIRECTORY){
+         pid=fork();
+         if(pid==0)
+          {
+           check_dir(new_path);
+           break;
+         }
+   }
+         else (type==FILE_PATH){
+           printf("oi?\n");
+           parse_file(path);
+           free(new_path);
+           continue;
+         }
+
+      }
      }
 
+  free(new_path);
+  closedir(dr);
 
-     free(new_dir);
-     closedir(dr);
-   return 0;
+  return 0;
 }
